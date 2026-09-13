e8dc0c633ac236f3125fa7047ac1e49203f38854
ddwukeep <326033172+ddwukeep@users.noreply.github.com>
2026-09-14 00:46:35 +0800

    Initial KOOCHI ENGINE website

---
+++ sources/data/lab.js
@@ -0,0 +1,80 @@
+export default [
+    {
+        title: 'Black Hole',
+        url: '',
+        image: 'black-hole.ktx',
+        imageMini: 'black-hole-mini.ktx'
+    },
+    {
+        title: 'Infinite World',
+        url: '',
+        image: 'infinite-world.ktx',
+        imageMini: 'infinite-world-mini.ktx'
+    },
+    {
+        title: 'My Room in 3D',
+        url: '',
+        image: 'my-room-in-3d.ktx',
+        imageMini: 'my-room-in-3d-mini.ktx',
+    },
+    {
+        title: 'Particles System',
+        url: '',
+        image: 'particles-system.ktx',
+        imageMini: 'particles-system-mini.ktx'
+    },
+    {
+        title: 'Stylized Low Poly',
+        url: '',
+        image: 'stylized-low-poly.ktx',
+        imageMini: 'stylized-low-poly-mini.ktx'
+    },
+    {
+        title: 'Holographic terrain',
+        url: '',
+        image: 'holographic-terrain.ktx',
+        imageMini: 'holographic-terrain-mini.ktx'
+    },
+    {
+        title: 'Woodkid Volcano Robot',
+        url: '',
+        image: 'woodkid-volcano-robot.ktx',
+        imageMini: 'woodkid-volcano-robot-mini.ktx'
+    },
+    {
+        title: 'Bounce Friday',
+        url: '',
+        image: 'bounce-friday.ktx',
+        imageMini: 'bounce-friday-mini.ktx'
+    },
+    {
+        title: 'VFX flames',
+        url: '',
+        image: 'vfx-flames.ktx',
+        imageMini: 'vfx-flames-mini.ktx'
+    },
+    {
+        title: 'VFX tornado',
+        url: '',
+        image: 'vfx-tornado.ktx',
+        imageMini: 'vfx-tornado-mini.ktx'
+    },
+    {
+        title: 'DOOM Portal',
+        url: '',
+        image: 'doom-portal.ktx',
+        imageMini: 'doom-portal-mini.ktx'
+    },
+    {
+        title: 'Organic Sphere',
+        url: '',
+        image: 'organic-sphere.ktx',
+        imageMini: 'organic-sphere-mini.ktx'
+    },
+    {
+        title: 'Attractors',
+        url: '',
+        image: 'attractors.ktx',
+        imageMini: 'attractors-mini.ktx'
+    }
+]
